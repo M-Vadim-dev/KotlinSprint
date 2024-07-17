2bb0cd5f3461db674a5a95a5aaf4faacab8a974d
M-Vadim-dev <railer@bk.ru>
2024-07-17 21:59:44 +0300

    KS-1-3

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,11 @@
+package lesson_1
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+    println("Год полета – $year\nВзлет – 0$hour:0$minute")
+    hour = 10
+    minute = 55
+    println("Посадка – $hour:$minute")
+}

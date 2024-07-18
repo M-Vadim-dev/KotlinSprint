b9c0b4e18a8b3d7337c6ef84b57e514c305730cf
M-Vadim-dev <railer@bk.ru>
2024-07-18 20:14:55 +0300

    KS-1-3 string

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,11 +1,11 @@
 package lesson_1
 
 fun main() {
-    val year: Int = 1961
-    var hour: Int = 9
-    var minute: Int = 7
-    println("Год полета – $year\nВзлет – 0$hour:0$minute")
-    hour = 10
-    minute = 55
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
+    println("Год полета – $year\nВзлет – $hour:$minute")
+    hour = "10"
+    minute = "55"
     println("Посадка – $hour:$minute")
 }

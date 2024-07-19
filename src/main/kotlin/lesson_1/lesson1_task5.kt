b97ae7e1917041a6a2c88215be3f8b560fa35c9a
M-Vadim-dev <railer@bk.ru>
2024-07-19 12:34:18 +0300

    KS-1-5 rename

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,12 @@
 package lesson_1
 
-const val TOTAL_SECONDS = 6489
 const val MINUTES = 60
 
 fun main() {
-    val seconds = TOTAL_SECONDS % MINUTES
-    val minute = (TOTAL_SECONDS / MINUTES) % MINUTES
-    val hour = (TOTAL_SECONDS / (MINUTES * MINUTES))
+    val totalSeconds = 6489
+    val seconds = totalSeconds % MINUTES
+    val minute = (totalSeconds / MINUTES) % MINUTES
+    val hour = (totalSeconds / (MINUTES * MINUTES))
     val hoursAndMinutesAndSeconds = "%02d:%02d:%02d".format(hour, minute, seconds)
     println("Время, проведенное в космосе: $hoursAndMinutesAndSeconds")
 }

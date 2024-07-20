3feb8e85be4f4f483c142c0eb964b882a66899b7
M-Vadim-dev <railer@bk.ru>
2024-07-20 17:48:20 +0300

    KS-2-4

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,27 +1,15 @@
 package lesson_2
 
+const val HUNDRED_PERCENT = 100
+
 fun main() {
     val miningCrystal = 7
     val miningIron = 11
-    val buff = 0.2
-    val buffActivityCrystal = true
-    val buffActivityIron = true
+    val buff = 20
 
-    val miningCrystalBuff = miningCrystal + buff(miningCrystal, buff)
-    val onlyMiningCrystalBuff = miningCrystalBuff - miningCrystal
-    val miningIronBuff = miningIron + buff(miningIron, buff)
-    val onlyMiningIronBuff = miningIronBuff - miningCrystal
+    val miningCrystalBuff = (miningCrystal * buff / HUNDRED_PERCENT)
+    val miningIronBuff = (miningIron * buff / HUNDRED_PERCENT)
 
-    if (buffActivityCrystal) {
-        println("При использовании баффа увеличивает добычу кристаллической руды на ${onlyMiningCrystalBuff.toInt()}")
-    } else println("Добыто $miningCrystal кристаллической руды.")
-
-    if (buffActivityIron) {
-        println("При использовании баффа увеличивает добычу железной руды на ${onlyMiningIronBuff.toInt()}")
-    } else println("Добыто $miningIron железной руды.")
-
-}
-
-fun buff(x: Int, y: Double): Double {
-    return x * y
+    println("Кристаллы: $miningCrystalBuff ")
+    println("Железная руда: $miningIronBuff")
 }

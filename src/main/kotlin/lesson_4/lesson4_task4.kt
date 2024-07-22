5711dd719934ddd0d660c0af7b34f3e472edde2b
M-Vadim-dev <railer@bk.ru>
2024-07-23 00:20:29 +0300

    KS-4-4 delete variable

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,13 +3,12 @@ package lesson_4
 fun main() {
     val trainingDay = 5
 
-    val armsAndAbsExercises = (trainingDay % 2) == 1   // Нечетные дни: руки и пресс
-    val legsAndBackExercises = (trainingDay % 2) == 0   // Четные дни: ноги и спина
+    val legsAndBackExercises = (trainingDay % 2) != 0
 
     println(
-        """Упражнения для рук:    $armsAndAbsExercises
-Упражнения для ног:    $legsAndBackExercises
-Упражнения для спины:  $legsAndBackExercises
-Упражнения для пресса: $armsAndAbsExercises"""
+        """Упражнения для рук:    $legsAndBackExercises
+Упражнения для ног:    ${!legsAndBackExercises}
+Упражнения для спины:  ${!legsAndBackExercises}
+Упражнения для пресса: $legsAndBackExercises"""
     )
 }

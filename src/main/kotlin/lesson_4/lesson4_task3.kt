b82357c125de936af22a2fdbca799ddd4e52ea54
M-Vadim-dev <railer@bk.ru>
2024-07-23 00:47:22 +0300

    KS-4-3 create const

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,12 +1,18 @@
 package lesson_4
 
+const val REQUIRED_HUMIDITY = 20
+const val SEASON = "весна"
+const val IS_SUNNY_WEATHER = true
+const val IS_TENT_OPEN = true
+
 fun main() {
     val isSunnyWeather: Boolean = true
     val isTentOpen: Boolean = true
     val humidity: Int = 20
     val currentSeason: String = "зима"
 
-    val favorableConditions: Boolean = isSunnyWeather && isTentOpen && (humidity == 20) && (currentSeason != "зима")
+    val favorableConditions: Boolean =
+        IS_SUNNY_WEATHER && IS_TENT_OPEN && (humidity == REQUIRED_HUMIDITY) && (currentSeason == SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $favorableConditions")
 }

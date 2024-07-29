b13c630ce95ff7ea001671f381b4ab4cb7611eb9
M-Vadim-dev <railer@bk.ru>
2024-07-29 09:33:58 +0300

    KS-6-2 приостановка на заданное количество секунд

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -3,31 +3,9 @@ package lesson_6
 fun main() {
     println("Введите количество секунд для таймера:")
     val secondsInput = readln().toInt()
-    var secondsPassed = 0
-    var pause = false
 
-    println("Для паузы таймера нажмите Enter.")
+    Thread.sleep(secondsInput * 1000L)
 
-    while (secondsPassed < secondsInput) {
-        if (!pause) {
-            Thread.sleep(1000)
-            secondsPassed++
-
-            // Проверка ввода для приостановки таймера
-            if (System.`in`.available() > 0) {
-                readln()
-                pause = true
-                println("Таймер приостановлен. Нажмите Enter для продолжения.")
-            }
-        } else {
-            // Проверка ввода для продолжения
-            if (System.`in`.available() > 0) {
-                readln()
-                pause = false
-                println("Таймер продолжен.")
-            }
-        }
-    }
     println("Прошло $secondsInput секунд.")
 }
 

7c9d2cc9dd1d2e6f9a94911e6aa7a3eb236561f1
M-Vadim-dev <railer@bk.ru>
2024-07-29 13:54:44 +0300

    KS-7-5 генератор пароля, минимальная длина пароля 6

---
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -0,0 +1,27 @@
+package lesson_7
+
+fun main() {
+    val letters = 'a'..'z'
+    val capitalizeLetters = 'A'..'Z'
+    val digits = '0'..'9'
+    var rangeInput: Int
+    val minPasswordLength = 6
+
+    do {
+        print("Введите длину пароля (не менее 6 символов): ")
+        rangeInput = readln().toInt()
+        if (rangeInput < minPasswordLength) {
+            println("Ошибка: Длина пароля должна быть не менее 6 символов!")
+        }
+    } while (rangeInput < minPasswordLength)
+
+    val combined = letters + digits + capitalizeLetters
+    var randomPassword = ""
+    val startIndex = 1
+
+    for (i in startIndex..rangeInput) {
+        val randomIndex = (Math.random() * combined.size).toInt()
+        randomPassword += combined[randomIndex]
+    }
+    println("Сгенерированный пароль: $randomPassword")
+}

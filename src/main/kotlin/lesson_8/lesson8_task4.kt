8185504f57a1d3ea5297433c941afea6c6822978
M-Vadim-dev <railer@bk.ru>
2024-07-30 23:25:02 +0300

    KS-8-4 замена ингредиента

---
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -0,0 +1,21 @@
+package lesson_8
+
+fun main() {
+    val ingredients = arrayOf("морковь", "лук", "картофель", "мясо", "свекла", "капуста")
+    println("Список ингредиентов: ${ingredients.joinToString(", ")}")
+
+    println("Введите ингредиент, который хотите заменить: ")
+    val ingredientReplace = readln().lowercase()
+
+    if (!ingredients.contains(ingredientReplace)) println("Такого ингредиента в списке нет.")
+    else println("Введите новый ингредиент: ")
+
+    val newIngredient = readln().lowercase()
+
+    val updateIngredients = ingredients.map {
+        if (it == ingredientReplace) newIngredient
+        else it
+    }
+
+    println("Готово! Вы сохранили следующий список: ${updateIngredients.joinToString(", ")}")
+}

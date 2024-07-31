1cfe0f75de3fc8053945ad606e849f9c3f9a851c
M-Vadim-dev <railer@bk.ru>
2024-07-31 08:30:45 +0300

    KS-8-4 indexOf

--- src/main/kotlin/lesson_8/lesson8_task4.kt
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -7,15 +7,16 @@ fun main() {
     println("Введите ингредиент, который хотите заменить: ")
     val ingredientReplace = readln().lowercase()
 
-    if (!ingredients.contains(ingredientReplace)) println("Такого ингредиента в списке нет.")
-    else println("Введите новый ингредиент: ")
+    val index = ingredients.indexOf(ingredientReplace)
+    if (index == -1) println("Такого ингредиента в списке нет.")
+    else {
+        println("Введите новый ингредиент: ")
+        val newIngredient = readln().lowercase()
 
-    val newIngredient = readln().lowercase()
-
-    val updateIngredients = ingredients.map {
-        if (it == ingredientReplace) newIngredient
-        else it
+        val updateIngredients = ingredients.map {
+            if (it == ingredientReplace) newIngredient
+            else it
+        }
+        println("Готово! Вы сохранили следующий список: ${updateIngredients.joinToString(", ")}")
     }
-
-    println("Готово! Вы сохранили следующий список: ${updateIngredients.joinToString(", ")}")
 }

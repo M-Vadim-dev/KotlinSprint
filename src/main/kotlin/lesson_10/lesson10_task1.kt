cacf99671cb6f6b98c806ae197b57ba2aac39e4c
M-Vadim-dev <railer@bk.ru>
2024-08-01 21:52:21 +0300

    KS-10-1  игра: бросок кости

--- src/main/kotlin/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -1,2 +1,22 @@
-package org.example.lesson_10
+package lesson_10
 
+fun main() {
+    val playerRoll = rollDice()
+    val computerRoll = rollDice()
+
+    println("Игрок бросает кости и получает: $playerRoll")
+    println("Компьютер бросает кости и получает: $computerRoll")
+
+    when {
+        playerRoll > computerRoll -> println("Победило человечество!")
+        computerRoll > playerRoll -> println("Победила машина!")
+        else -> println("Ничья!")
+    }
+}
+
+const val MIN_DICE_VALUE = 1
+const val MAX_DICE_VALUE = 6
+
+fun rollDice(): Int {
+    return (MIN_DICE_VALUE..MAX_DICE_VALUE).random()
+}

1eed89472baed832b3774697be7de33c73f24377
M-Vadim-dev <railer@bk.ru>
2024-08-03 23:18:47 +0300

    KS-12-1 create class WeatherData

---
+++ src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -0,0 +1,32 @@
+package lesson_12
+
+class WeatherData(
+    var daytimeTemperature: Float = 20.0f,
+    var nighttimeTemperature: Float = 10.0f,
+    var precipitation: Float = 0.0f,
+) {
+    fun displayWeather() {
+        println("Дневная температура: $daytimeTemperature °C")
+        println("Ночная температура: $nighttimeTemperature °C")
+        println("Осадки: $precipitation мм")
+    }
+}
+
+fun main() {
+    val weatherDay1 = WeatherData()
+    val weatherDay2 = WeatherData()
+
+    weatherDay1.daytimeTemperature = 25.0f
+    weatherDay1.nighttimeTemperature = 15.0f
+    weatherDay1.precipitation = 5.0f
+
+    weatherDay2.daytimeTemperature = 30.0f
+    weatherDay2.nighttimeTemperature = 20.0f
+    weatherDay2.precipitation = 0.0f
+
+    println("Погода на день 1:")
+    weatherDay1.displayWeather()
+
+    println("\nПогода на день 2:")
+    weatherDay2.displayWeather()
+}

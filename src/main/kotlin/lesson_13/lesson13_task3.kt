e644f4e37665e80542097821d885f6af9b1cef04
M-Vadim-dev <railer@bk.ru>
2024-08-06 01:48:34 +0300

    KS-13-3

---
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -0,0 +1,25 @@
+package lesson_13
+
+class ContactDirectory(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+)
+
+fun main() {
+    val contacts = listOf(
+        ContactDirectory("Ростислав", 89123456789, "Reddit"),
+        ContactDirectory("Анна", 89123456780, null),
+        ContactDirectory("Иван", 89123456781, "null"),
+        ContactDirectory("Елена", 89123456782, "GitHub"),
+        ContactDirectory("Сергей", 89123456783, null)
+    )
+    printUniqueCompanies(contacts)
+}
+
+fun printUniqueCompanies(contacts: List<ContactDirectory>) {
+    val uniqueCompanies = contacts.mapNotNull { it.company }
+
+    val printUniqueCompanies = uniqueCompanies.joinToString(", ")
+    println("Все существующие компании: $printUniqueCompanies")
+}

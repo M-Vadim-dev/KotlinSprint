dfc1196d8129e4c17c565ae7e0d733a70bb0bd36
M-Vadim-dev <railer@bk.ru>
2024-08-11 16:24:17 +0300

    KS-14-5 добавил main

--- src/main/kotlin/lesson_14/lesson14_task5.kt
+++ src/main/kotlin/lesson_14/lesson14_task5.kt
@@ -1,50 +1,49 @@
 package lesson_14
 
-open class Message(
-    val id: String,
-    val author: String,
-    val text: String
-)
+open class Message(val id: String, val author: String, val text: String)
 
 class ChildMessage(
     id: String,
     author: String,
     text: String,
-    val parentMessageId: String
+    val parentMessageId: String,
 ) : Message(id, author, text)
 
 class Chat {
     private val messages = mutableListOf<Message>()
 
-    fun addMessage(text: String, author: String) {
+    fun addMessage(text: String, author: String): String {
         val messageId = generateId()
         val message = Message(messageId, author, text)
         messages.add(message)
+        return messageId
     }
 
     fun addThreadMessage(text: String, author: String, parentMessageId: String) {
         val messageId = generateId()
-        val childMessage = ChildMessage(messageId, author, text, parentMessageId)
-        messages.add(childMessage)
+        messages.add(ChildMessage(messageId, author, text, parentMessageId))
     }
 
     fun printChat() {
-        val groupedMessages = messages.groupBy {
-            when (it) {
-                is ChildMessage -> it.parentMessageId
-                else -> it.id
-            }
-        }
-
-        groupedMessages.forEach { (key, msgs) ->
-            msgs.forEach { msg ->
-                when (msg) {
-                    is Message -> println("Message [ID: ${msg.id}, Author: ${msg.author}]: ${msg.text}")
-                    is ChildMessage -> println("ChildMessage [ID: ${msg.id}, Author: ${msg.author}]: ${msg.text}")
+        messages.groupBy { if (it is ChildMessage) it.parentMessageId else it.id }
+            .forEach { (key, msgs) ->
+                msgs.forEach { msg ->
+                    println("${if (msg is ChildMessage) "ChildMessage" else "Message"} [ID: ${msg.id}, Автор: ${msg.author}]: ${msg.text}")
                 }
             }
-        }
     }
 
     private fun generateId() = System.currentTimeMillis().toString() + (messages.size + 1)
+}
+
+fun main() {
+    val chat = Chat()
+
+    val message1Id = chat.addMessage("Привет всем!", "Вадим")
+    val message2Id = chat.addMessage("Как дела?", "Мария")
+
+    chat.addThreadMessage("Привет", "Федор", message1Id)
+    chat.addThreadMessage("Отлично, а у тебя?", "Игорь", message2Id)
+
+    chat.printChat()
 }

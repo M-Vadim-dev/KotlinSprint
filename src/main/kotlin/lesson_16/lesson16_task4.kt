7816420c7b985233016227c325869f670f09ab33
M-Vadim-dev <railer@bk.ru>
2024-08-11 16:36:49 +0300

    KS-16-4 статус заказа

---
+++ src/main/kotlin/lesson_16/lesson16_task4.kt
@@ -0,0 +1,42 @@
+package lesson_16
+
+class Order(private val orderNumber: String) {
+    private var status: String = "Новый"
+
+    fun requestStatusChange(newStatus: String) {
+        if (sendRequestToManager(newStatus)) {
+            updateStatus(newStatus)
+        }
+    }
+
+    private fun sendRequestToManager(newStatus: String): Boolean {
+        println("Заявка на изменение статуса на \"$newStatus\".")
+        return true
+    }
+
+    private fun updateStatus(newStatus: String) {
+        status = newStatus
+        println("Статус заказа обновлён на \"$status\".")
+    }
+
+    fun getOrderNumber(): String {
+        return orderNumber
+    }
+
+    fun getStatus(): String {
+        return status
+    }
+}
+
+fun main() {
+    val order = Order("111")
+
+    println("Номер заказа: ${order.getOrderNumber()}")
+    println("Текущий статус: ${order.getStatus()}")
+
+    order.requestStatusChange("В обработке")
+    println("Обновлённый статус: ${order.getStatus()}")
+
+    order.requestStatusChange("Завершён")
+    println("Обновлённый статус: ${order.getStatus()}")
+}

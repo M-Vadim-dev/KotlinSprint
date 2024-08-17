a631c6fcadf7ce1f0739563cd53da18e49cd1c0f
M-Vadim-dev <railer@bk.ru>
2024-08-18 01:33:52 +0300

    KS-19-3-describe-unrealized-methods

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,16 @@
+package lesson_19
+
+class Spaceship {
+
+    fun takeOff() {
+        TODO("Реализовать метод взлёта")
+    }
+
+    fun land() {
+        // TODO: Реализовать метод приземления
+    }
+
+    fun shootOffAsteroid() {
+        throw NotImplementedError("Метод стрельбы еще не реализован.")
+    }
+}
